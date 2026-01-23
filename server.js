7d5369c89433e4cf62b01cd5ec3770f913e631a7
ZetX1998 <sten.alpha.d@gmail.com>
2026-01-23 22:36:10 +0100

    Update account number in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.post("/create-qr", (req, res) => {
   const randomCents = Math.floor(Math.random() * 90 + 10);
   const finalAmount = (baseAmount + randomCents / 100).toFixed(2);
 
-  const account = "CZ6508000000001234567899"; // TVŮJ ÚČET
+  const account = "CZ37 30300000002906469015"; // TVŮJ ÚČET
   const message = "VeritasAI";
 
   const spd = `SPD*1.0*ACC:${account}*AM:${finalAmount}*CC:CZK*MSG:${message}`;
